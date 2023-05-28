906e59abb2598c5b1fadc73cab06be0579f1fd08
Phelipe Lucas <phlucasfr@gmail.com>
2023-05-28 14:40:24 -0300

    Fix MVVM pattern.

---
+++ GameSphere/User.swift
@@ -0,0 +1,15 @@
+//
+//  UserAuth.swift
+//  GameSphere
+//
+//  Created by Phelipe Lucas on 27/05/23.
+//
+
+import Foundation
+
+struct User {
+      
+    let email:String
+    let password:String
+        
+}
